b3db5ff884472c3ec8cb8f191351544b13186835
Keenan Keeling <kkeeling@automatastudios.com>
2021-09-10 16:19:21 -0400

    rts

--- django/lottery/nfl/tasks.py
+++ django/lottery/nfl/tasks.py
@@ -805,6 +805,10 @@ def process_projection_sheet(sheet_id, task_id):
                 site=sheet.slate.site
             )
 
+            if sheet.projection_site == 'rts':
+                headers.column_player_name = csv_reader.fieldnames[0]
+                headers.save()
+
             for row in csv_reader:
                 player_name = row[headers.column_player_name]
                 team = 'JAC' if row[headers.column_team] == 'JAX' else row[headers.column_team]
