ee41852f1b27c4d3d2ff0e85b17b7ad0a2924928
Keenan Keeling <kkeeling@automatastudios.com>
2021-10-15 22:25:37 -0400

    rating lineups

--- django/lottery/nfl/admin.py
+++ django/lottery/nfl/admin.py
@@ -1302,8 +1302,8 @@ class SlateBuildGroupAdmin(admin.ModelAdmin):
 
 @admin.register(models.SlateBuildLineup, site=lottery_admin_site)
 class SlateBuildLineupAdmin(admin.ModelAdmin):
-    list_per_page = 25
-    paginator = NoCountPaginator
+    list_per_page = 10
+    # paginator = NoCountPaginator
     list_display = (
         'stack',
         'get_stack_rank',
