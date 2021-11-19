67e5a807b79f796de10ea6c3401b909dde0f5820
Keenan Keeling <kkeeling@automatastudios.com>
2021-11-19 11:48:37 -0500

    more sims

---
+++ django/lottery/scripts/fanduel_correlation_matrix.py
@@ -0,0 +1,136 @@
+import math
+import numpy as np
+import pandas as pd
+import scipy
+import traceback
+
+from django.db.models import F
+
+from nfl import models
+
+
+def run():
+    qbs = find_qbs()
+    matrix = []
+
+    for index, qb in enumerate(qbs):
+        rbs = find_players(qb, 'RB', 2)
+        if rbs.count() < 2:
+            continue
+        wrs = find_players(qb, 'WR', 3)
+        if wrs.count() < 3:
+            continue
+        tes = find_players(qb, 'TE', 1)
+        if tes.count() < 1:
+            continue
+        dsts = find_players(qb, 'D', 1)
+        if dsts.count() < 1:
+            continue
+
+        o_qbs = find_qbs(qb)
+        if o_qbs.count() < 1:
+            continue
+        o_rbs = find_players(qb, 'RB', 2, find_opponent=True)
+        if o_rbs.count() < 2:
+            continue
+        o_wrs = find_players(qb, 'WR', 3, find_opponent=True)
+        if o_wrs.count() < 3:
+            continue
+        o_tes = find_players(qb, 'TE', 1, find_opponent=True)
+        if o_tes.count() < 1:
+            continue
+        o_dsts = find_players(qb, 'D', 1, find_opponent=True)
+        if o_dsts.count() < 1:
+            continue
+
+        row = [
+            qb.fantasy_points,
+            rbs[0].fantasy_points,
+            rbs[1].fantasy_points,
+            wrs[0].fantasy_points,
+            wrs[1].fantasy_points,
+            wrs[2].fantasy_points,
+            tes[0].fantasy_points,
+            dsts[0].fantasy_points,
+            o_qbs[0].fantasy_points,
+            o_rbs[0].fantasy_points,
+            o_rbs[1].fantasy_points,
+            o_wrs[0].fantasy_points,
+            o_wrs[1].fantasy_points,
+            o_wrs[2].fantasy_points,
+            o_tes[0].fantasy_points,
+            o_dsts[0].fantasy_points,
+        ]
+
+        matrix.append(row)
+
+    v = pd.DataFrame(matrix, dtype=float, columns=[
+        'qb',
+        'rb1',
+        'rb2',
+        'wr1',
+        'wr2',
+        'wr3',
+        'te',
+        'dst',
+        'opp qb',
+        'opp rb1',
+        'opp rb2',
+        'opp wr1',
+        'opp wr2',
+        'opp wr3',
+        'opp te',
+        'opp dst',
+    ])
+
+    r = v.corr(method='pearson')
+    r.to_csv(f'data/r.csv')
+
+
+def find_qbs(qb=None):
+    '''
+    Query DB for relevant QBs.
+
+    If qb parameter is used, find opposing qb
+    '''
+    if qb is None:
+        qbs = models.SlatePlayer.objects.filter(
+            slate__site='fanduel',
+            site_pos='QB',
+            projection__projection__gt=9.9,
+            fantasy_points__gt=4.9,
+            slate_game__isnull=False,
+            slate__is_main_slate=True
+        ).select_related('projection').annotate(proj=F('projection__projection'))
+    else:
+        qbs = models.SlatePlayer.objects.filter(
+            slate=qb.slate,
+            site_pos='QB',
+            projection__projection__gt=9.9,
+            fantasy_points__gt=4.9,
+            team=qb.get_opponent()
+        ).select_related(
+            'projection'
+        ).annotate(
+            proj=F('projection__projection')
+        )
+    
+    return qbs
+
+
+def find_players(qb, position, depth, find_opponent=False):
+    team = qb.get_opponent() if find_opponent else qb.team
+    players = models.SlatePlayer.objects.filter(
+        slate=qb.slate,
+        site_pos=position,
+        team=team,
+        projection__isnull=False
+    ).select_related(
+        'projection'
+    ).annotate(
+        proj=F('projection__projection')
+    ).order_by('-proj')
+
+    if players.count() < depth:
+        return players
+    return players[:depth]
--- django/lottery/scripts/simulate_nfl_game.py
+++ django/lottery/scripts/simulate_nfl_game.py
@@ -9,7 +9,7 @@ from nfl import models
 
 def run():
     r_df = pd.read_csv('data/nfl_correl_matrix_no_k_test.csv', index_col=0)
-    c = scipy.linalg.cholesky(r_df, lower=True)
+    c = scipy.linalg.cholesky(r_df)
 
     slate = models.Slate.objects.filter(week__slate_year=2021, week__num=5, site='fanduel').order_by('?')[0]
 
@@ -57,43 +57,43 @@ def run():
             away_dst_scores = []
 
             for _ in range(0, 10000):
-                x = [
-                    np.random.gamma((float(home_qb.projection)/float(home_qb.stdev))**2, (float(home_qb.stdev)**2)/float(home_qb.projection)),
-                    np.random.gamma((float(home_rb1.projection)/float(home_rb1.stdev))**2, (float(home_rb1.stdev)**2)/float(home_rb1.projection)),
-                    np.random.gamma((float(home_rb2.projection)/float(home_rb2.stdev))**2, (float(home_rb2.stdev)**2)/float(home_rb2.projection)),
-                    np.random.gamma((float(home_wr1.projection)/float(home_wr1.stdev))**2, (float(home_wr1.stdev)**2)/float(home_wr1.projection)),
-                    np.random.gamma((float(home_wr2.projection)/float(home_wr2.stdev))**2, (float(home_wr2.stdev)**2)/float(home_wr2.projection)),
-                    np.random.gamma((float(home_wr3.projection)/float(home_wr3.stdev))**2, (float(home_wr3.stdev)**2)/float(home_wr3.projection)),
-                    np.random.gamma((float(home_te.projection)/float(home_te.stdev))**2, (float(home_te.stdev)**2)/float(home_te.projection)),
-                    np.random.gamma((float(home_dst.projection)/float(home_dst.stdev))**2, (float(home_dst.stdev)**2)/float(home_dst.projection)),
-                    np.random.gamma((float(away_qb.projection)/float(away_qb.stdev))**2, (float(away_qb.stdev)**2)/float(away_qb.projection)),
-                    np.random.gamma((float(away_rb1.projection)/float(away_rb1.stdev))**2, (float(away_rb1.stdev)**2)/float(away_rb1.projection)),
-                    np.random.gamma((float(away_rb2.projection)/float(away_rb2.stdev))**2, (float(away_rb2.stdev)**2)/float(away_rb2.projection)),
-                    np.random.gamma((float(away_wr1.projection)/float(away_wr1.stdev))**2, (float(away_wr1.stdev)**2)/float(away_wr1.projection)),
-                    np.random.gamma((float(away_wr2.projection)/float(away_wr2.stdev))**2, (float(away_wr2.stdev)**2)/float(away_wr2.projection)),
-                    np.random.gamma((float(away_wr3.projection)/float(away_wr3.stdev))**2, (float(away_wr3.stdev)**2)/float(away_wr3.projection)),
-                    np.random.gamma((float(away_te.projection)/float(away_te.stdev))**2, (float(away_te.stdev)**2)/float(away_te.projection)),
-                    np.random.gamma((float(away_dst.projection)/float(away_dst.stdev))**2, (float(away_dst.stdev)**2)/float(away_dst.projection)),
-                ]
-
                 # x = [
-                #     scipy.stats.foldnorm.rvs(2.286320653446043, loc=0.009521750045927459, scale=0.43647078822917096, size=1)[0] * float(home_qb.projection),
-                #     scipy.stats.gengamma.rvs(0.8569512382187675, 1.7296589884149502, loc=0.1696436245041962, scale=1.0034840685805952, size=1)[0] * float(home_rb1.projection),
-                #     scipy.stats.gengamma.rvs(0.8569512382187675, 1.7296589884149502, loc=0.1696436245041962, scale=1.0034840685805952, size=1)[0] * float(home_rb2.projection),
-                #     scipy.stats.geninvgauss.rvs(1.5027595619420469, 1.08441508981995, loc=0.1396719464795535, scale=0.30497399320969815, size=1)[0] * float(home_wr1.projection),
-                #     scipy.stats.geninvgauss.rvs(1.5027595619420469, 1.08441508981995, loc=0.1396719464795535, scale=0.30497399320969815, size=1)[0] * float(home_wr2.projection),
-                #     scipy.stats.geninvgauss.rvs(1.5027595619420469, 1.08441508981995, loc=0.1396719464795535, scale=0.30497399320969815, size=1)[0] * float(home_wr3.projection),
-                #     scipy.stats.beta.rvs(1.3211694111775993, 8.619168174695513, loc=0.05324597661516427, scale=7.141266442024949, size=1)[0] * float(home_te.projection),
-                #     scipy.stats.burr12.rvs(35.758034183278085, 0.4498053645571314, loc=-11.318659107435849, scale=11.704623152048967, size=1)[0] * float(home_dst.projection),
-                #     scipy.stats.foldnorm.rvs(2.286320653446043, loc=0.009521750045927459, scale=0.43647078822917096, size=1)[0] * float(away_qb.projection),
-                #     scipy.stats.gengamma.rvs(0.8569512382187675, 1.7296589884149502, loc=0.1696436245041962, scale=1.0034840685805952, size=1)[0] * float(away_rb1.projection),
-                #     scipy.stats.gengamma.rvs(0.8569512382187675, 1.7296589884149502, loc=0.1696436245041962, scale=1.0034840685805952, size=1)[0] * float(away_rb2.projection),
-                #     scipy.stats.geninvgauss.rvs(1.5027595619420469, 1.08441508981995, loc=0.1396719464795535, scale=0.30497399320969815, size=1)[0] * float(away_wr1.projection),
-                #     scipy.stats.geninvgauss.rvs(1.5027595619420469, 1.08441508981995, loc=0.1396719464795535, scale=0.30497399320969815, size=1)[0] * float(away_wr2.projection),
-                #     scipy.stats.geninvgauss.rvs(1.5027595619420469, 1.08441508981995, loc=0.1396719464795535, scale=0.30497399320969815, size=1)[0] * float(away_wr3.projection),
-                #     scipy.stats.beta.rvs(1.3211694111775993, 8.619168174695513, loc=0.05324597661516427, scale=7.141266442024949, size=1)[0] * float(away_te.projection),
-                #     scipy.stats.burr12.rvs(35.758034183278085, 0.4498053645571314, loc=-11.318659107435849, scale=11.704623152048967, size=1)[0] * float(away_dst.projection),
+                #     np.random.gamma((float(home_qb.projection)/float(home_qb.stdev))**2, (float(home_qb.stdev)**2)/float(home_qb.projection)),
+                #     np.random.gamma((float(home_rb1.projection)/float(home_rb1.stdev))**2, (float(home_rb1.stdev)**2)/float(home_rb1.projection)),
+                #     np.random.gamma((float(home_rb2.projection)/float(home_rb2.stdev))**2, (float(home_rb2.stdev)**2)/float(home_rb2.projection)),
+                #     np.random.gamma((float(home_wr1.projection)/float(home_wr1.stdev))**2, (float(home_wr1.stdev)**2)/float(home_wr1.projection)),
+                #     np.random.gamma((float(home_wr2.projection)/float(home_wr2.stdev))**2, (float(home_wr2.stdev)**2)/float(home_wr2.projection)),
+                #     np.random.gamma((float(home_wr3.projection)/float(home_wr3.stdev))**2, (float(home_wr3.stdev)**2)/float(home_wr3.projection)),
+                #     np.random.gamma((float(home_te.projection)/float(home_te.stdev))**2, (float(home_te.stdev)**2)/float(home_te.projection)),
+                #     np.random.gamma((float(home_dst.projection)/float(home_dst.stdev))**2, (float(home_dst.stdev)**2)/float(home_dst.projection)),
+                #     np.random.gamma((float(away_qb.projection)/float(away_qb.stdev))**2, (float(away_qb.stdev)**2)/float(away_qb.projection)),
+                #     np.random.gamma((float(away_rb1.projection)/float(away_rb1.stdev))**2, (float(away_rb1.stdev)**2)/float(away_rb1.projection)),
+                #     np.random.gamma((float(away_rb2.projection)/float(away_rb2.stdev))**2, (float(away_rb2.stdev)**2)/float(away_rb2.projection)),
+                #     np.random.gamma((float(away_wr1.projection)/float(away_wr1.stdev))**2, (float(away_wr1.stdev)**2)/float(away_wr1.projection)),
+                #     np.random.gamma((float(away_wr2.projection)/float(away_wr2.stdev))**2, (float(away_wr2.stdev)**2)/float(away_wr2.projection)),
+                #     np.random.gamma((float(away_wr3.projection)/float(away_wr3.stdev))**2, (float(away_wr3.stdev)**2)/float(away_wr3.projection)),
+                #     np.random.gamma((float(away_te.projection)/float(away_te.stdev))**2, (float(away_te.stdev)**2)/float(away_te.projection)),
+                #     np.random.gamma((float(away_dst.projection)/float(away_dst.stdev))**2, (float(away_dst.stdev)**2)/float(away_dst.projection)),
                 # ]
+
+                x = [
+                    scipy.stats.foldnorm.rvs(2.286320653446043, loc=0.009521750045927459, scale=0.43647078822917096, size=1)[0] * float(home_qb.projection),
+                    scipy.stats.gengamma.rvs(0.8569512382187675, 1.7296589884149502, loc=0.1696436245041962, scale=1.0034840685805952, size=1)[0] * float(home_rb1.projection),
+                    scipy.stats.gengamma.rvs(0.8569512382187675, 1.7296589884149502, loc=0.1696436245041962, scale=1.0034840685805952, size=1)[0] * float(home_rb2.projection),
+                    scipy.stats.geninvgauss.rvs(1.5027595619420469, 1.08441508981995, loc=0.1396719464795535, scale=0.30497399320969815, size=1)[0] * float(home_wr1.projection),
+                    scipy.stats.geninvgauss.rvs(1.5027595619420469, 1.08441508981995, loc=0.1396719464795535, scale=0.30497399320969815, size=1)[0] * float(home_wr2.projection),
+                    scipy.stats.geninvgauss.rvs(1.5027595619420469, 1.08441508981995, loc=0.1396719464795535, scale=0.30497399320969815, size=1)[0] * float(home_wr3.projection),
+                    scipy.stats.beta.rvs(1.3211694111775993, 8.619168174695513, loc=0.05324597661516427, scale=7.141266442024949, size=1)[0] * float(home_te.projection),
+                    scipy.stats.burr12.rvs(35.758034183278085, 0.4498053645571314, loc=-11.318659107435849, scale=11.704623152048967, size=1)[0] * float(home_dst.projection),
+                    scipy.stats.foldnorm.rvs(2.286320653446043, loc=0.009521750045927459, scale=0.43647078822917096, size=1)[0] * float(away_qb.projection),
+                    scipy.stats.gengamma.rvs(0.8569512382187675, 1.7296589884149502, loc=0.1696436245041962, scale=1.0034840685805952, size=1)[0] * float(away_rb1.projection),
+                    scipy.stats.gengamma.rvs(0.8569512382187675, 1.7296589884149502, loc=0.1696436245041962, scale=1.0034840685805952, size=1)[0] * float(away_rb2.projection),
+                    scipy.stats.geninvgauss.rvs(1.5027595619420469, 1.08441508981995, loc=0.1396719464795535, scale=0.30497399320969815, size=1)[0] * float(away_wr1.projection),
+                    scipy.stats.geninvgauss.rvs(1.5027595619420469, 1.08441508981995, loc=0.1396719464795535, scale=0.30497399320969815, size=1)[0] * float(away_wr2.projection),
+                    scipy.stats.geninvgauss.rvs(1.5027595619420469, 1.08441508981995, loc=0.1396719464795535, scale=0.30497399320969815, size=1)[0] * float(away_wr3.projection),
+                    scipy.stats.beta.rvs(1.3211694111775993, 8.619168174695513, loc=0.05324597661516427, scale=7.141266442024949, size=1)[0] * float(away_te.projection),
+                    scipy.stats.burr12.rvs(35.758034183278085, 0.4498053645571314, loc=-11.318659107435849, scale=11.704623152048967, size=1)[0] * float(away_dst.projection),
+                ]
 
                 # x = [
                 #     scipy.stats.skewnorm.rvs(1.4132959978590718, loc=float(home_qb.projection), scale=float(home_qb.stdev)),
