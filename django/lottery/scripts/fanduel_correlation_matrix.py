8e384c40d07bc8bf5998ebbbbde46238c6de18da
Keenan Keeling <kkeeling@automatastudios.com>
2022-01-29 21:27:02 -0500

    fixed fd correlation matrix

--- django/lottery/scripts/fanduel_correlation_matrix.py
+++ django/lottery/scripts/fanduel_correlation_matrix.py
@@ -11,36 +11,35 @@ from nfl import models
 
 def run():
     qbs = find_qbs()
-    print(f'there are {qbs.count()} qbs')
     matrix = []
 
     for index, qb in enumerate(qbs):
-        rbs = find_players(qb, 'RB', 2)
-        if rbs.count() < 2:
+        rbs = find_players(qb, 'RB', 3)
+        if rbs.count() < 3:
             continue
-        wrs = find_players(qb, 'WR', 3)
-        if wrs.count() < 3:
+        wrs = find_players(qb, 'WR', 5)
+        if wrs.count() < 5:
             continue
-        tes = find_players(qb, 'TE', 1)
-        if tes.count() < 1:
+        tes = find_players(qb, 'TE', 2)
+        if tes.count() < 2:
             continue
-        dsts = find_players(qb, 'D', 1)
+        dsts = find_players(qb, 'DST', 1)
         if dsts.count() < 1:
             continue
 
         o_qbs = find_qbs(qb)
         if o_qbs.count() < 1:
             continue
-        o_rbs = find_players(qb, 'RB', 2, find_opponent=True)
-        if o_rbs.count() < 2:
+        o_rbs = find_players(qb, 'RB', 3, find_opponent=True)
+        if o_rbs.count() < 3:
             continue
-        o_wrs = find_players(qb, 'WR', 3, find_opponent=True)
-        if o_wrs.count() < 3:
+        o_wrs = find_players(qb, 'WR', 5, find_opponent=True)
+        if o_wrs.count() < 5:
             continue
-        o_tes = find_players(qb, 'TE', 1, find_opponent=True)
-        if o_tes.count() < 1:
+        o_tes = find_players(qb, 'TE', 2, find_opponent=True)
+        if o_tes.count() < 2:
             continue
-        o_dsts = find_players(qb, 'D', 1, find_opponent=True)
+        o_dsts = find_players(qb, 'DST', 1, find_opponent=True)
         if o_dsts.count() < 1:
             continue
 
@@ -48,18 +47,26 @@ def run():
             qb.fantasy_points,
             rbs[0].fantasy_points,
             rbs[1].fantasy_points,
+            rbs[2].fantasy_points,
             wrs[0].fantasy_points,
             wrs[1].fantasy_points,
             wrs[2].fantasy_points,
+            wrs[3].fantasy_points,
+            wrs[4].fantasy_points,
             tes[0].fantasy_points,
+            tes[1].fantasy_points,
             dsts[0].fantasy_points,
             o_qbs[0].fantasy_points,
             o_rbs[0].fantasy_points,
             o_rbs[1].fantasy_points,
+            o_rbs[2].fantasy_points,
             o_wrs[0].fantasy_points,
             o_wrs[1].fantasy_points,
             o_wrs[2].fantasy_points,
+            o_wrs[3].fantasy_points,
+            o_wrs[4].fantasy_points,
             o_tes[0].fantasy_points,
+            o_tes[1].fantasy_points,
             o_dsts[0].fantasy_points,
         ]
 
@@ -69,23 +76,32 @@ def run():
         'qb',
         'rb1',
         'rb2',
+        'rb3',
         'wr1',
         'wr2',
         'wr3',
-        'te',
+        'wr4',
+        'wr5',
+        'te1',
+        'te2',
         'dst',
         'opp qb',
         'opp rb1',
         'opp rb2',
+        'opp rb3',
         'opp wr1',
         'opp wr2',
         'opp wr3',
-        'opp te',
+        'opp wr4',
+        'opp wr5',
+        'opp te1',
+        'opp te2',
         'opp dst',
     ])
 
     r = v.corr(method='pearson')
-    r.to_csv(f'data/fd_r_prime.csv')
+    print(r)
+    r.to_csv(f'data/dk_r.csv')
 
 
 def find_qbs(qb=None):
@@ -98,12 +114,10 @@ def find_qbs(qb=None):
         qbs = models.SlatePlayer.objects.filter(
             slate__site='fanduel',
             site_pos='QB',
-            # projection__projection__gt=9.9,
-            # fantasy_points__gt=4.9,
+            projection__projection__gt=9.9,
+            fantasy_points__gt=4.9,
             slate_game__isnull=False,
-            slate__is_main_slate=True,
-            fantasy_points__isnull=False,
-            fantasy_points__gte=25.0
+            slate__is_main_slate=True
         ).select_related('projection').annotate(proj=F('projection__projection'))
     else:
         qbs = models.SlatePlayer.objects.filter(
