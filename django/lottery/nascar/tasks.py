d0f82dc3e6eaadf72550748080f37adcedb2b98b
Keenan Keeling <kkeeling@automatastudios.com>
2022-03-04 10:57:56 -0500

    bugs

--- django/lottery/nascar/tasks.py
+++ django/lottery/nascar/tasks.py
@@ -1076,7 +1076,7 @@ def execute_sim_iteration(sim_id):
         fl_rank_vals.append(randrange(0, driver_fl[i]+1) + random())
     fl_ranks = len(fl_rank_vals) + 1 - scipy.stats.rankdata(fl_rank_vals, method='ordinal')
 
-    # -- Finally, award FL to drivers based on FL ranks
+    # -- Finally, award LL to drivers based on FL ranks
     ll_laps_remaining = ll_laps
     ll_laps_assigned = []
     profiles = list(race_sim.ll_profiles.all().order_by('rank_order'))
@@ -1103,7 +1103,7 @@ def execute_sim_iteration(sim_id):
             ll_index = randrange(1, 21)
 
         # sp_index = int(numpy.where(final_ranks == ll_index)[0][0])
-        ll_val = max(ll_laps_remaining, 5)
+        ll_val = min(ll_laps_remaining, 5)
         driver_ll[ll_index] += ll_val
         ll_laps_assigned.append(ll_index)
 
