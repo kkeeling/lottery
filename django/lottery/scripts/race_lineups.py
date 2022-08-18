3648b36083672b5b94cd0d4bcc98b07d30aa45f7
Keenan Keeling <kkeeling@automatastudios.com>
2022-08-17 20:45:04 -0400

    process optimizations

--- django/lottery/scripts/race_lineups.py
+++ django/lottery/scripts/race_lineups.py
@@ -18,10 +18,13 @@ def run():
 
     start = time.time()
     projections = build.projections.filter(in_play=True).order_by('-slate_player__salary')
+    player_outcomes = pandas.DataFrame.from_records(projections.values('slate_player_id', 'sim_scores'), index=[p.slate_player.slate_player_id for p in projections])
     # player_outcomes = pandas.DataFrame([p.sim_scores for p in projections], index=[p.slate_player.slate_player_id for p in projections], dtype='float16')
-    player_outcomes = {}
-    for p in projections:
-        player_outcomes[p.slate_player.slate_player_id] = numpy.array(p.sim_scores)
+    # player_outcomes['slate_player_id'] = [p.slate_player.slate_player_id for p in projections]
+    print(player_outcomes)
+    # player_outcomes = {}
+    # for p in projections:
+    #     player_outcomes[p.slate_player.slate_player_id] = numpy.array(p.sim_scores)
     print(f'Getting player outcomes took {time.time() - start}s')
 
     start = time.time()
@@ -39,94 +42,69 @@ def run():
     slate_lineups = filters.SlateLineupFilter(models.BUILD_TYPE_FILTERS.get(build.build_type), possible_lineups).qs.order_by('id')
     print(f'Filtered slate lineups took {time.time() - start}s')
     
-    start = time.time()
+    # start = time.time()
     df_slate_lineups = pandas.DataFrame(slate_lineups.values_list('player_1', 'player_2', 'player_3', 'player_4', 'player_5', 'player_6'), index=list(slate_lineups.values_list('id', flat=True)))
-    df_slate_lineups['build_id'] = build.id
+    # df_slate_lineups['build_id'] = build.id
     df_slate_lineups['slate_lineup_id'] = df_slate_lineups.index
-    df_slate_lineups = df_slate_lineups.apply(pandas.to_numeric, downcast='unsigned')
+    # df_slate_lineups = df_slate_lineups.apply(pandas.to_numeric, downcast='unsigned')
     print(f'  Initial dataframe took {time.time() - start}s')
-
+    # print(df_slate_lineups)
+    # print(f'{player_outcomes.get(str(df_slate_lineups.loc[2009202, 0]))[0]} + {player_outcomes.get(str(df_slate_lineups.loc[2009202, 1]))[0]} + {player_outcomes.get(str(df_slate_lineups.loc[2009202, 2]))[0]} + {player_outcomes.get(str(df_slate_lineups.loc[2009202, 3]))[0]} + {player_outcomes.get(str(df_slate_lineups.loc[2009202, 4]))[0]} + {player_outcomes.get(str(df_slate_lineups.loc[2009202, 5]))[0]}')
+    # print(f'{player_outcomes.get(str(df_slate_lineups.loc[2009202, 0]))[1]} + {player_outcomes.get(str(df_slate_lineups.loc[2009202, 1]))[1]} + {player_outcomes.get(str(df_slate_lineups.loc[2009202, 2]))[1]} + {player_outcomes.get(str(df_slate_lineups.loc[2009202, 3]))[1]} + {player_outcomes.get(str(df_slate_lineups.loc[2009202, 4]))[1]} + {player_outcomes.get(str(df_slate_lineups.loc[2009202, 5]))[1]}')
     start = time.time()
-    df_slate_lineups['score_1'] = df_slate_lineups[0].map(lambda x: player_outcomes.get(str(x))).apply(pandas.to_numeric, downcast='float')
-    df_slate_lineups['score_2'] = df_slate_lineups[1].map(lambda x: player_outcomes.get(str(x))).apply(pandas.to_numeric, downcast='float')
-    df_slate_lineups['score_3'] = df_slate_lineups[2].map(lambda x: player_outcomes.get(str(x))).apply(pandas.to_numeric, downcast='float')
-    df_slate_lineups['score_4'] = df_slate_lineups[3].map(lambda x: player_outcomes.get(str(x))).apply(pandas.to_numeric, downcast='float')
-    df_slate_lineups['score_5'] = df_slate_lineups[4].map(lambda x: player_outcomes.get(str(x))).apply(pandas.to_numeric, downcast='float')
-    df_slate_lineups['score_6'] = df_slate_lineups[5].map(lambda x: player_outcomes.get(str(x))).apply(pandas.to_numeric, downcast='float')
-    df_slate_lineups['scores'] = (df_slate_lineups['score_1'] + df_slate_lineups['score_2'] + df_slate_lineups['score_3'] + df_slate_lineups['score_4'] + df_slate_lineups['score_5'] + df_slate_lineups['score_6']).apply(pandas.to_numeric, downcast='float')
-    df_slate_lineups = df_slate_lineups.drop([
-        'score_1',
-        'score_2',
-        'score_3',
-        'score_4',
-        'score_5',
-        'score_6',
-    ], axis=1)
-    print(df_slate_lineups.info(verbose=True, memory_usage='deep'))
-    print(df_slate_lineups)
-    # df_slate_lineups = df_slate_lineups.apply(lambda x: player_outcomes.get(str(x[0])) + player_outcomes.get(str(x[1])) + player_outcomes.get(str(x[2])) + player_outcomes.get(str(x[3])) + player_outcomes.get(str(x[4])) + player_outcomes.get(str(x[5])), axis=1, result_type='expand')
+    p1 = df_slate_lineups.merge(player_outcomes, how='left', left_on=0, right_on='slate_player_id')#.merge(player_outcomes, how='inner', left_on=1, right_on='slate_player_id').merge(player_outcomes, how='inner', left_on=2, right_on='slate_player_id').merge(player_outcomes, how='inner', left_on=3, right_on='slate_player_id').merge(player_outcomes, how='inner', left_on=4, right_on='slate_player_id').merge(player_outcomes, how='inner', left_on=5, right_on='slate_player_id')
+    print(f'  Merge took {time.time() - start}s')
+    start = time.time()
+    p2 = df_slate_lineups.merge(player_outcomes, how='left', left_on=1, right_on='slate_player_id')
+    print(f'  Merge took {time.time() - start}s')
+    start = time.time()
+    p3 = df_slate_lineups.merge(player_outcomes, how='left', left_on=2, right_on='slate_player_id')
+    print(f'  Merge took {time.time() - start}s')
+    start = time.time()
+    p4 = df_slate_lineups.merge(player_outcomes, how='left', left_on=3, right_on='slate_player_id')
+    print(f'  Merge took {time.time() - start}s')
+    start = time.time()
+    p5 = df_slate_lineups.merge(player_outcomes, how='left', left_on=4, right_on='slate_player_id')
+    print(f'  Merge took {time.time() - start}s')
+    start = time.time()
+    p6 = df_slate_lineups.merge(player_outcomes, how='left', left_on=5, right_on='slate_player_id')
+    print(f'  Merge took {time.time() - start}s')
+    start = time.time()
+    print(p1)
+    print(p2)
+    print(p3)
+    print(p4)
+    print(p5)
+    print(p6)
+    df = pandas.DataFrame(numpy.array(p1['sim_scores'].to_list()) + numpy.array(p2['sim_scores'].to_list()) + numpy.array(p3['sim_scores'].to_list()) + numpy.array(p4['sim_scores'].to_list()) + numpy.array(p5['sim_scores'].to_list()) + numpy.array(p6['sim_scores'].to_list()), index=list(slate_lineups.values_list('id', flat=True)))
+    # df.to_csv('data/slate_lineups.csv')
+    print(df)
+    # print(player_outcomes.query('slate_player_id in ["23136059", "23136060", "23136061", "23136077", "23136096", "23136098"]').sum(numeric_only=True))
+    # print(df_slate_lineups.apply(lambda x: player_outcomes.query(f'slate_player_id in ["{str(x[0])}", "{str(x[1])}", "{str(x[2])}", "{str(x[3])}", "{str(x[4])}", "{str(x[5])}"]').sum(numeric_only=True), axis=1))
+    # df_slate_lineups = df_slate_lineups.apply(lambda x: player_outcomes.loc[str(x[0])] + player_outcomes.loc[str(x[1])] + player_outcomes.loc[str(x[2])] + player_outcomes.loc[str(x[3])] + player_outcomes.loc[str(x[4])] + player_outcomes.loc[str(x[5])], axis=1, result_type='expand')
     # df_slate_lineups = df_slate_lineups.apply(pandas.to_numeric, downcast='float')
-    # print(df_slate_lineups.info(verbose=True, memory_usage='deep'))
+    # print(df_slate_lineups.loc[2009202])
+    print(df_slate_lineups)
     print(f'  Sim scores took {time.time() - start}s')
 
-    start = time.time()
-    field_lineups = build.field_lineups.all().order_by('id')
-    print(f'Getting field lineups took {time.time() - start}s.')
-    start = time.time()
-    df_field_lineups = pandas.DataFrame(field_lineups.values_list('slate_lineup__player_1', 'slate_lineup__player_2', 'slate_lineup__player_3', 'slate_lineup__player_4', 'slate_lineup__player_5', 'slate_lineup__player_6'), index=list(field_lineups.values_list('id', flat=True)))
-    df_field_lineups = df_field_lineups.apply(pandas.to_numeric, downcast='unsigned')
-    print(f'  Initial dataframe took {time.time() - start}s')
-    start = time.time()
-    df_field_lineups['score_1'] = df_field_lineups[0].map(lambda x: player_outcomes.get(str(x))).apply(pandas.to_numeric, downcast='float')
-    df_field_lineups['score_2'] = df_field_lineups[1].map(lambda x: player_outcomes.get(str(x))).apply(pandas.to_numeric, downcast='float')
-    df_field_lineups['score_3'] = df_field_lineups[2].map(lambda x: player_outcomes.get(str(x))).apply(pandas.to_numeric, downcast='float')
-    df_field_lineups['score_4'] = df_field_lineups[3].map(lambda x: player_outcomes.get(str(x))).apply(pandas.to_numeric, downcast='float')
-    df_field_lineups['score_5'] = df_field_lineups[4].map(lambda x: player_outcomes.get(str(x))).apply(pandas.to_numeric, downcast='float')
-    df_field_lineups['score_6'] = df_field_lineups[5].map(lambda x: player_outcomes.get(str(x))).apply(pandas.to_numeric, downcast='float')
-    df_field_lineups['scores'] = (df_field_lineups['score_1'] + df_field_lineups['score_2'] + df_field_lineups['score_3'] + df_field_lineups['score_4'] + df_field_lineups['score_5'] + df_field_lineups['score_6']).apply(pandas.to_numeric, downcast='float')
-    df_field_lineups = df_field_lineups.drop([
-        'score_1',
-        'score_2',
-        'score_3',
-        'score_4',
-        'score_5',
-        'score_6',
-    ], axis=1)
+    # start = time.time()
+    # field_lineups = build.field_lineups.all().order_by('id')
+    # print(f'Getting field lineups took {time.time() - start}s.')
+    # start = time.time()
+    # df_field_lineups = pandas.DataFrame(field_lineups.values_list('slate_lineup__player_1', 'slate_lineup__player_2', 'slate_lineup__player_3', 'slate_lineup__player_4', 'slate_lineup__player_5', 'slate_lineup__player_6'), index=list(field_lineups.values_list('id', flat=True)))
+    # df_field_lineups = df_field_lineups.apply(pandas.to_numeric, downcast='unsigned')
+    # print(f'  Initial dataframe took {time.time() - start}s')
+    # start = time.time()
     # df_field_lineups = df_field_lineups.apply(lambda x: player_outcomes.get(str(x[0])) + player_outcomes.get(str(x[1])) + player_outcomes.get(str(x[2])) + player_outcomes.get(str(x[3])) + player_outcomes.get(str(x[4])) + player_outcomes.get(str(x[5])), axis=1, result_type='expand')
     # df_field_lineups = df_field_lineups.apply(pandas.to_numeric, downcast='float')
-    print(df_field_lineups.info(verbose=True, memory_usage='deep'))
-    print(df_field_lineups)
-    print(f'  Sim scores took {time.time() - start}s')
+    # print(f'  Sim scores took {time.time() - start}s')
 
-    start = time.time()
-    matchups  = list(itertools.product(slate_lineups.values_list('id', flat=True), field_lineups.values_list('id', flat=True)))
-    df_matchups = pandas.DataFrame(matchups, columns=['slate_lineup_id', 'field_lineup_id'])
-
-    slate_lineup_scores = df_matchups['slate_lineup_id'].map(lambda x: numpy.array(df_slate_lineups.loc[x]['scores'])).to_numpy()
-    field_scores = df_matchups['field_lineup_id'].map(lambda x: numpy.array(df_field_lineups.loc[x]['scores'])).to_numpy()
-    diffs = slate_lineup_scores - field_scores
-    del slate_lineup_scores
-    del field_lineups
-    gc.collect()
-    
-    df_matchups['win_rate'] = [numpy.count_nonzero(x > 0.0) / build.sim.iterations for x in diffs]
-    del diffs
-    gc.collect()
-
-    # df_matchups['diffs'] = numpy.array(df_matchups['slate_lineup_scores']) - numpy.array(df_matchups['field_scores'])
-    # df_matchups = df_matchups.drop([
-    #     'slate_lineup_scores',
-    #     'field_scores'
-    # ], axis=1)
-    # df_matchups['win_rate'] = df_matchups['diffs'].map(lambda x: numpy.count_nonzero(x > 0.0) / build.sim.iterations).apply(pandas.to_numeric, downcast='float')
-    # df_matchups = df_matchups.drop([
-    #     'diffs'
-    # ], axis=1)
-    # # df_matchups['win_rate'] = df_matchups.apply(lambda x: numpy.count_nonzero((numpy.array(df_slate_lineups.loc[x['slate_lineup_id']]) - numpy.array(df_field_lineups.loc[x['field_lineup_id']])) > 0.0) / build.sim.iterations, axis=1)
-    df_matchups = df_matchups[(df_matchups.win_rate >= 0.58)]
-    df_matchups['build_id'] = build.id
+    # start = time.time()
+    # matchups  = list(itertools.product(slate_lineups.values_list('id', flat=True), field_lineups.values_list('id', flat=True)))
+    # df_matchups = pandas.DataFrame(matchups, columns=['slate_lineup_id', 'field_lineup_id'])
+    # df_matchups['win_rate'] = df_matchups.apply(lambda x: numpy.count_nonzero((numpy.array(df_slate_lineups.loc[x['slate_lineup_id']]) - numpy.array(df_field_lineups.loc[x['field_lineup_id']])) > 0.0) / build.sim.iterations, axis=1)
+    # df_matchups = df_matchups[(df_matchups.win_rate >= 0.58)]
+    # df_matchups['build_id'] = build.id
     # df_matchups = df_matchups.apply(pandas.to_numeric, downcast='float')
-    print(df_matchups.info(verbose=True, memory_usage='deep'))
-    print(df_matchups)
-    print(f'Matchups took {time.time() - start}s. There are {len(matchups)} matchups.')
+    # print(f'Matchups took {time.time() - start}s. There are {len(matchups)} matchups.')
 
