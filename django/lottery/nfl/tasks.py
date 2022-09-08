ce155aedb997fd0d918ba34d9b30b402fd74800b
Keenan Keeling <kkeeling@automatastudios.com>
2022-09-08 07:52:34 -0700

    nfl szn

--- django/lottery/nfl/tasks.py
+++ django/lottery/nfl/tasks.py
@@ -859,15 +859,15 @@ def compare_lineups_h2h(lineup_ids, build_id):
     logger.info(f'Matchups took {time.time() - start}s. There are {df_matchups.size} matchups.')
 
     start = time.time()
-    user = settings.DATABASES['default']['USER']
-    password = settings.DATABASES['default']['PASSWORD']
-    database_name = settings.DATABASES['default']['NAME']
-    database_url = 'postgresql://{user}:{password}@db:5432/{database_name}'.format(
-        user=user,
-        password=password,
-        database_name=database_name,
-    )
-    engine = sqlalchemy.create_engine(database_url, echo=False)
+    # user = settings.DATABASES['default']['USER']
+    # password = settings.DATABASES['default']['PASSWORD']
+    # database_name = settings.DATABASES['default']['NAME']
+    # database_url = 'postgresql://{user}:{password}@db:5432/{database_name}'.format(
+    #     user=user,
+    #     password=password,
+    #     database_name=database_name,
+    # )
+    # engine = sqlalchemy.create_engine(database_url, echo=False)
     df_matchups.to_sql('nfl_lineupmatchup', engine, if_exists='append', index=False, chunksize=1000)
     logger.info(f'Write matchups to db took {time.time() - start}s')
 
