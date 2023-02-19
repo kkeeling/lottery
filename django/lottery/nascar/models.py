5e462c67ab37667b275ce452b7d875dc55df049c
Keenan Keeling <kkeeling@automatastudios.com>
2023-02-19 11:52:05 -0500

    allow rp racing

--- django/lottery/nascar/models.py
+++ django/lottery/nascar/models.py
@@ -120,16 +120,16 @@ BUILD_TYPES = (
 
 BUILD_TYPE_FILTERS = {
     'cash': {
-        'total_salary__gte': 48500
+        'total_salary__gte': 36500
     },
     'h2h': {
-        'total_salary__gte': 48500
+        'total_salary__gte': 36500
     },
     'se': {
-        'total_salary__gte': 48500
+        'total_salary__gte': 36500
     },
     'mme': {
-        'total_salary__gte': 48500
+        'total_salary__gte': 36500
     }
 }
 
